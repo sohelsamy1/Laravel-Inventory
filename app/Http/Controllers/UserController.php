9bd4402db1b193add756a30682b9bb74b019140d
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2025-11-28 21:49:03 +0600

    work on controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function userRegistrationPage(){
-        return view('pages.auth.registration-page');
-    }
+    public function userRegistration(Request $request){
+        try{
+            User::create([
+           'first_name' => $request->first_name,
+           'last_name' => $request->last_name,
+           'email' => $request->email,
+           'mobile' => $request->mobile,
+           'password' => $request->password,
+            ]);
+            return response()->json([
+                'status' => 'success',
+                 'message' => 'User Registration successful'
+            ], 200);
+        }catch(\Throwable $e){
+
+       }
+     }
 
 
        public function userloginPage(){
